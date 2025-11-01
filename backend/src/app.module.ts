2199cf203b617d54f1bda357242f6afedfc54cd3
guilhermyandrade <gw.andrade@hotmail.com>
2025-10-31 21:56:30 -0300

    first commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SupabaseModule } from './supabase/supabase.module';
+import { UserModule } from './cases/users/user.module';
+import { AuthModule } from './cases/auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASS,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    SupabaseModule,
+    AuthModule,
+    UserModule
+
+  ],
+})
+export class AppModule {}
