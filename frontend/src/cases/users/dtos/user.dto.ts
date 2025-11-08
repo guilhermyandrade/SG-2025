58bce1388aedcc6c127eea1cfbeec1ac2ce91ce1
guilhermyandrade <gw.andrade@hotmail.com>
2025-11-07 21:59:29 -0300

    Aula 07/11

---
+++ frontend/src/cases/users/dtos/user.dto.ts
@@ -0,0 +1,10 @@
+export interface UserDTO {
+    id?: string;
+    name: string;
+    email: string;
+    supabaseId: string;
+    hasMFA: boolean;
+    factorId: string;
+    isMFAValidated?: boolean;
+    isFirstMFAAccess?: boolean;
+}
